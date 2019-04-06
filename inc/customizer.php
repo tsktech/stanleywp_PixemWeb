b6af6f5e99271120e2bb24fe0047ac358b1e8ab2
Srikanth Kamath <Srikanth@tskamath.com>
2019-04-06 18:08:51 +0530

    first commit

---
+++ inc/customizer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * StrapPress Theme Customizer
+ *
+ * @package StrapPress
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function strappress_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'strappress_customize_register' );
